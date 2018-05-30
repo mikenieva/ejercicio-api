590c204feea26c5278fd769551d75622f9060fc6
Mike Nieva <miguelnieva@conexionew.com>
2018-05-30 14:29:47 -0500

    JSON

--- server.js
+++ server.js
@@ -9,14 +9,11 @@ var express = require('express'),
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/Tododb'); 
 
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-
 var routes = require('./api/routes/todoListRoutes'); //importing route
 routes(app); //register the route
-
 
 app.listen(port);
 
@@ -24,5 +21,5 @@ app.use(function(req, res) {
     res.status(404).send({url: req.originalUrl + ' not found'})
   });
 
-
 console.log('todo list RESTful API server started on: ' + port);
+
